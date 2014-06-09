b22422b3fe6e3f66df6a947019352ddd4f1c06d3
Tim <tim@tim-desktop.(none)>
2014-06-09 18:15:43 +0200

    Using scipy optimise

--- runAnalysis.py
+++ runAnalysis.py
@@ -12,12 +12,16 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn import linear_model, datasets, metrics
 from sklearn.neural_network import BernoulliRBM
 from sklearn.pipeline import Pipeline
+from scipy.optimize import minimize
 
 accuracy = 0.05
 ams_best = 0.0
 vars_best = ''
-file_dir = '/scratch/s1214155/htautau/'
+#file_dir = '/scratch/s1214155/htautau/'
+file_dir = '/media/swap/Htautau/'
 logfile = open(file_dir+sys.argv[1]+'runAnalysis.log','w')
+nEvents = 0
+solutionFile = file_dir+"solutions.csv"
 # Need an adapter to give the gradientboostclassifier a decisiontree as a parameter
 # https://stackoverflow.com/questions/17454139/gradientboostingclassifier-with-a-baseestimator-in-scikit-learn/19679862#19679862
 class BaseTree:
@@ -30,7 +34,7 @@ class BaseTree:
 
 # write out the solution file in the correct format
 def solnFile(fname, cls, eventid, bkg=None):
-    global file_dir
+    global file_dir, nEvents
     f = open(file_dir+fname+'.out','w')
     # need to figure out the rankorder still
    
@@ -39,6 +43,7 @@ def solnFile(fname, cls, eventid, bkg=None):
     for x in xrange(0,len(cls)):
         f.write(str(eventid[x])+','+str(count)+','+str(cls[x])+'\n')
         count+=1
+    nEvents = count-1
     if bkg:
         for x,r in bkg.iterrows():
             f.write(str(r['EventId'])+','+str(count)+',b\n')
@@ -91,8 +96,10 @@ sigtest=test#[(test.DER_mass_transverse_met_lep < 70) & (test.DER_mass_vis > 30)
 
 
 # DecisionTreeClassifier http://scikit-learn.org/stable/modules/tree.html#tree-classification
-def runDTree(depth, filename):
-    clf = tree.DecisionTreeClassifier(max_depth=depth)
+def runDTree(depth):#, filename):
+    global file_dir, nEvents, solutionFile
+    #filename = 
+    clf = tree.DecisionTreeClassifier(max_depth=depth[0])
     print "DecisionTreeClassifier training"
     clf = clf.fit(sigtr[train_input].values, sigtr['Label'].values)
     print "DecisionTreeClassifier testing"
@@ -102,8 +109,14 @@ def runDTree(depth, filename):
 
 # Ensemble methods http://scikit-learn.org/stable/modules/ensemble.html
 # AdaBoostClassifier
-def runAdaBoost(depth, n_est, filename, lrn_rate=1.0):
+def runAdaBoost(arr):#depth, n_est,  lrn_rate=1.0): # removing filename for the scipy optimise thing '''filename,'''
     #ada = AdaBoostClassifier(n_estimators=100)
+    global file_dir, nEvents, solutionFile
+    depth = int(arr[0]*10)
+    n_est = int(arr[1]*100)
+    lrn_rate = arr[2]
+    fname = 'ada_dep'+str(depth)+'_est'+str(n_est)+'_lrn'+str(lrn_rate)
+    filename = fname
     ada = AdaBoostClassifier(tree.DecisionTreeClassifier(max_depth=depth),
                              algorithm="SAMME",
                              n_estimators=n_est)#,n_jobs=4)
@@ -113,9 +126,18 @@ def runAdaBoost(depth, n_est, filename, lrn_rate=1.0):
     ada_pred = ada.predict(sigtest[train_input].values)
     solnFile(filename,ada_pred,sigtest['EventId'].values)#
     print "AdaBoost finished"
+    # added for teh scipy optimise thing
+    ams_score = ams.AMS_metric(solutionFile, file_dir+fname+'.out', nEvents)
+    logfile.write(fname + ': ' + str(ams_score))
+    return -1.0*float(ams_score) # since we are minimising
 
 # http://scikit-learn.org/stable/auto_examples/ensemble/plot_adaboost_multiclass.html#example-ensemble-plot-adaboost-multiclass-py
-def runAdaReal(depth, n_est, filename, lrn_rate=1.0):
+def runAdaReal(arr):#depth, n_est, filename, lrn_rate=1.0):
+    global file_dir, nEvents, solutionFile
+    depth = arr[0]
+    n_est = arr[1]
+    lrn_rate = arr[2]
+    filename =  'adar_dep'+str(depth)+'_est'+str(n_est)+'_lrn'+str(lrn_rate) # low
     bdt_real = AdaBoostClassifier(
         tree.DecisionTreeClassifier(max_depth=depth),
         n_estimators=n_est,
@@ -126,18 +148,37 @@ def runAdaReal(depth, n_est, filename, lrn_rate=1.0):
     bdt_real_pred = bdt_real.predict(sigtest[train_input].values)
     solnFile(filename,bdt_real_pred,sigtest['EventId'].values)#
     print "AdaBoostReal finished"
+    ams_score = ams.AMS_metric(solutionFile, file_dir+filename+'.out', nEvents)
+    logfile.write(filename+': ' + str(ams_score))
+    return -1.0*float(ams_score)
+                  
 
 # GradientBoostingClassifier
-def runGDB(depth, n_est, filename, lrn_rate=1.0):
+def runGDB(arr):#depth, n_est, filename, lrn_rate=1.0):
+    global file_dir, nEvents, solutionFile
+    depth = arr[0]
+    n_est = arr[1]
+    lrn_rate = arr[2]
+    filename =  'gdb_dep'+str(depth)+'_est'+str(n_est)+'_lrn'+str(lrn_rate) # low
     print "GDB training"
     gdb = GradientBoostingClassifier(init=BaseTree(tree.DecisionTreeClassifier(max_depth=depth)),n_estimators=n_est, learning_rate=lrn_rate, random_state=0).fit(sigtr[train_input].values, sigtr['Label'].values)
     print "GDB testing"
     gdb_pred = gdb.predict(sigtest[train_input].values)
     solnFile(filename,gdb_pred,sigtest['EventId'].values)#
     print "GDB finished"
+    ams_score = ams.AMS_metric(solutionFile, file_dir+filename+'.out', nEvents)
+    logfile.write(filename+': ' + str(ams_score))
+    return -1.0*float(ams_score)
 
 # ANN - http://scikit-learn.org/stable/auto_examples/plot_rbm_logistic_classification.html#example-plot-rbm-logistic-classification-py
-def runRBM(iters, lrn_rate, logistic_c_val, logistic_c_val2, n_comp, filename):
+def runRBM(arr, clsfr):#iters, lrn_rate, logistic_c_val, logistic_c_val2, n_comp, filename):
+    global file_dir, nEvents, solutionFile
+    iters = int(arr[0])
+    lrn_rate = arr[1]
+    logistic_c_val = arr[2]
+    logistic_c_val2 = arr[3]
+    n_comp = int(arr[4])
+    filename = 'rbm_iter'+str(iters)+'_logc'+str(log_c_val)+'_logcc'+str(log_c_val2)+'_lrn'+str(learn_rate)+'_nc'+str(n_comp)# low
     logistic = linear_model.LogisticRegression()
     rbm = BernoulliRBM(random_state=0, verbose=True)
     
@@ -165,16 +206,26 @@ def runRBM(iters, lrn_rate, logistic_c_val, logistic_c_val2, n_comp, filename):
 
     ###############################################################################
     # Evaluation
-    clsnn_pred=classifier.predict(sigtest[train_input].values)
-    solnFile('clsnn_'+filename,clsnn_pred,sigtest['EventId'].values)#,bkgtest)
+    if clsfr == 0:
+        clsnn_pred=classifier.predict(sigtest[train_input].values)
+        solnFile('clsnn_'+filename,clsnn_pred,sigtest['EventId'].values)#,bkgtest)
+        ams_score = ams.AMS_metric(solutionFile, file_dir+filename+'.out', nEvents)
+        logfile.write(filename+': ' + str(ams_score))
     
-    log_cls_pred = logistic_classifier.predict(sigtest[train_input].values)
-    solnFile('lognn_'+filename,log_cls_pred,sigtest['EventId'].values)#,bkgtest)
-    
-    logistic_classifier_tx = linear_model.LogisticRegression(C=logistic_c_val2)
-    logistic_classifier_tx.fit_transform(sigtr[train_input].values, sigtr['Label'].values)
-    log_cls_tx_pred = logistic_classifier_tx.predict(sigtest[train_input].values)
-    solnFile('lognntx_'+filename,log_cls_tx_pred,sigtest['EventId'].values)#,bkgtest)
+    elif clsfr == 1:
+        log_cls_pred = logistic_classifier.predict(sigtest[train_input].values)
+        solnFile('lognn_'+filename,log_cls_pred,sigtest['EventId'].values)#,bkgtest)
+        ams_score = ams.AMS_metric(solutionFile, file_dir+'lognn_'+filename+'.out', nEvents)
+        logfile.write('lognn ' + filename+': ' + str(ams_score))
+    else:
+        logistic_classifier_tx = linear_model.LogisticRegression(C=logistic_c_val2)
+        logistic_classifier_tx.fit_transform(sigtr[train_input].values, sigtr['Label'].values)
+        log_cls_tx_pred = logistic_classifier_tx.predict(sigtest[train_input].values)
+        solnFile('lognntx_'+filename,log_cls_tx_pred,sigtest['EventId'].values)#,bkgtest)
+        ams_score = ams.AMS_metric(solutionFile, file_dir+filename+'.out', nEvents)
+        logfile.write('lognntx '+ filename+': ' + str(ams_score))
+
+    return -1.0*float(ams_score)
 
 
 def maximiseScores(method, learn_rate, n_est, max_depth, iters, log_cval, log_cval2, n_comps):
@@ -208,128 +259,6 @@ def maximiseScores(method, learn_rate, n_est, max_depth, iters, log_cval, log_cv
     params['n_comps_high'] = 500
     params['n_comps'] = n_comps
     
-    global ams_best
-    global accuracy
-    global vars_best
-    ams_prev = 0.0
-    nEvents = 125000
-    # want to keep an overall best score, but best for each variable too!
-    max_iters = 15
-    # must check that parameter is being used for that method!
-    fname_low = 'notset'
-    fname_high = 'notset'
-    for key in params.keys():
-        if key.find('low') != -1 or key.find('high') != -1:
-            continue
-        keylo = key+'_low'
-        keyhi = key+'_high'
-        itercount = 0
-        optimal = False
-        changedHi = False
-
-        # need to set low and high values here before entering loop!!!!
-        temp = params[key]
-        params[key] = params[keylo]
-        ams_low = runAdaBoost(params['max_depth'], params['n_est'], fname_low , params['learn_rate']) # low
-        params[key] = params[keyhi]
-        ams_high = runAdaBoost(params['max_depth'], params['n_est'], fname_low , params['learn_rate']) # low
-        params[key] = temp
-
-        while not optimal and itercount < max_iters: # how many iterations to run before stopping???
-            
-            # need to put these in try / catch incase it crashes
-            if  (not method == 'RBM') and key in ['iters','log_cval','log_cval2','n_comps']:
-                optimal = True
-                continue
-            if method == 'RBM' and key in ['max_depth', 'n_est']:
-                optimal = True
-                continue
-
-            if method == 'AdaBoost':
-            #depth, n_est, filename, lrn_rate=1.0
-                if not changedHi:
-                    params[key] = params[keylo]
-                    fname_low = 'ada_dep'+str(params['max_depth'])+'_est'+str(params['n_est'])+'_lrn'+str(params['learn_rate'])
-                    runAdaBoost(params['max_depth'], params['n_est'], fname_low , params['learn_rate']) # low
-                if changedHi:
-                    params[key] = params[keyhi]
-                    fname_high = 'ada_dep'+str(params['max_depth'])+'_est'+str(params['n_est'])+'_lrn'+str(params['learn_rate'])
-                    runAdaBoost(params['max_depth'], params['n_est'], fname_high, params['learn_rate'])            
-            elif method == 'AdaReal':
-                if not changedHi:
-                    params[key] = params[keylo]
-                    fname_low =  'adar_dep'+str(params['max_depth'])+'_est'+str(params['n_est'])+'_lrn'+str(params['learn_rate']) # low
-                    runAdaReal(params['max_depth'], params['n_est'], fname_low, params['learn_rate'])
-                if changedHi:
-                    params[key] = params[keyhi]
-                    fname_high = 'adar_dep'+str(params['max_depth'])+'_est'+str(params['n_est'])+'_lrn'+str(params['learn_rate']) # high
-                    runAdaReal(params['max_depth'], params['n_est'], fname_high, params['learn_rate'])
-            elif method == 'GDB':
-                if not changedHi:
-                    params[key] = params[keylo]
-                    fname_low = 'gdb_dep'+str(params['max_depth'])+'_est'+str(params['n_est'])+'_lrn'+str(params['learn_rate']) # low
-                    runGDB(params['max_depth'], params['n_est'], fname_low, params['learn_rate']) # low
-                if changedHi:
-                    params[key] = params[keyhi]
-                    fname_high = 'gdb_dep'+str(params['max_depth'])+'_est'+str(params['n_est'])+'_lrn'+str(params['learn_rate']) # high
-                    runGDB(params['max_depth'], params['n_est'], fname_high, params['learn_rate'])
-            elif method == 'RBM':
-            #iters, lrn_rate, logistic_c_val, logistic_c_val2, n_comp
-                if not changedHi:
-                    params[key] = params[keylo]
-                    fname_low = 'rbm_iter'+str(params['iters'])+'_logc'+str(params['log_c_val'])+'_logcc'+str(params['log_c_val2'])+'_lrn'+str(params['learn_rate'])+'_nc'+str(params['n_comp'])# low
-                    runRBM(params['iters'], params['lrn_rate'], params['log_c_val'], params['log_c_val2'], params['n_comp'], fname_low) # low
-                if changedHi:
-                    params[key] = params[keyhi]
-                    fname_high = 'rbm_iter'+str(params['iters'])+'_logc'+str(params['log_c_val'])+'_logcc'+str(params['log_c_val2'])+'_lrn'+str(params['learn_rate'])+'_nc'+str(params['n_comp']) # low
-                    runRBM(params['iters'], params['lrn_rate'], params['log_c_val'], params['log_c_val2'], params['n_comp'], fname_high) # high
-
-            try:
-                print fname_high
-                ams_up = ams.AMS_metric(solutionFile, file_dir+fname_high+'.out', nEvents)
-            except:
-                print 'could not get AMS_metric for ams_up :('
-                iterations+=1
-                continue
-            try:
-                print fname_low
-                ams_do = ams.AMS_metric(solutionFile, file_dir+fname_low+'.out', nEvents)
-            except:
-                print 'could not get AMS_metric for ams_do :('
-                iterations+=1
-                continue
-            if ams_up >= ams_best:
-                ams_best = ams_up
-                vars_best = fname_high
-            if ams_do >= ams_best:
-                ams_best = ams_do
-                vars_best = fname_low
-
-
-            if ams_up < ams_do:
-                params[key] = (params[keyhi]+params[keylo])/2
-                changedHi = True
-            else:
-                params[key] = (params[keyhi]+params[keylo])/2
-                changedHi = False
-            if (1.0 - abs((float(params[keyhi] - params[keylo]))/float((params[keyhi]+params[keylo])))) % 1 <= accuracy: # within 5% of each other
-                optimal = True
-            # perhaps stop if the last x iterations have failed to produce better results??
-            ams_prev = max(ams_up,ams_do)
-            print 'ams_best: ' + str(ams_best)
-            print 'ams_up: ' + str(ams_up)
-            print 'ams_do: ' + str(ams_do)
-            itercount +=1
-            print 'Itercount: ' + str(itercount) + ' on variable ' + key
-            logfile.write('Itercount: ' + str(itercount)+ ' variable: ' + key+'\n')
-            logfile.write('ams_best: ' + str(ams_best)+' vars: ' + vars_best +'\n')
-            logfile.write('ams_up: ' + str(ams_up)+ ' vars: ' + fname_high + '\n')
-            logfile.write('ams_do: ' + str(ams_do)+ ' vars: ' + fname_low + '\n')
-            logfile.write('#####\n')
-
-    return [params['learn_rate'], params['n_est'], params['max_depth'], params['iters'], params['log_cval'], params['log_cval2'], params['n_comps']]
-
-
 
 learn_rate = 1.0
 n_est = 200
@@ -344,39 +273,42 @@ scores_acc = 0.02
 method = sys.argv[1]
 iterations = 0
 
-while cont and iterations < 10:
-    learn_rate_f, n_est_f, max_depth_f, iters_f, log_cval_f, log_cval2_f, n_comps_f = maximiseScores(method,learn_rate, n_est, max_depth, iters, log_cval, log_cval2, n_comps)
-    # if we see less than... some percentage change for all values, then stop
-    lrn_d = learn_rate_f/learn_rate
-    cont = False
-    if abs(1-lrn_d) % 1 > scores_acc: # 2% change
-        learn_rate = learn_rate_f
-        cont = True
-    n_est_d = n_est_f/n_est
-    if abs(1-n_est_d) % 1 > scores_acc:
-        n_est = n_est_f
-        cont = True
-    max_depth_d = max_depth_f/max_depth
-    if abs(1-max_depth_d)  % 1 > scores_acc:
-        max_depth = max_depth_f
-        cont = True
-    iters_d = iters_f/iters
-    if abs(1-iters_d) % 1 > scores_acc:
-        iters = iters_f
-        cont = True
-    log_cval_d = log_cval_f/log_cval
-    if abs(1-log_cval_d) % 1 > scores_acc:
-        log_cval = log_cval_f
-        cont = True
-    log_cval2_d = log_cval2_f/log_cval2
-    if abs(1-log_cval2_d) % 1 > scores_acc:
-        log_cval2 = log_cval2_f
-        cont = True
-    n_comps_d = n_comps_f/n_comps
-    if abs(1-n_comps_d) % 1 > scores_acc:
-        n_comps = n_comps_f
-        cont = True
-    iterations += 1
-    logfile.write('running iteration number '+ str(iterations)+'\n')
+lrn_rate = 1.0
+n_est = 2.00 #will *100 in method
+depth = 0.6 # will *10 in method
+
+x0ada = np.array([depth,n_est,lrn_rate])
+res_ada = minimize(runAdaBoost, x0ada, method='nelder-mead',options={'xtol': 1e-2, 'disp': True})
+print res_ada.x
+logfile.write(res_ada.x)
+
+x0adar = np.array([depth,n_est,lrn_rate])
+res_r = minimize(runAdaBoost, x0adar, method='nelder-mead',options={'xtol': 1e-2, 'disp': True})
+print res_r.x
+logfile.write(res_r.x)
+
+x0gdb = np.array([depth,n_est,lrn_rate])
+res_gdb = minimize(runGDB, x0gdb, method='nelder-mead',options={'xtol': 1e-2, 'disp': True})
+print res_gdb.x
+logfile.write(res_gdb.x)
+
+#iters, lrn_rate, logistic_c_val, logistic_c_val2, n_comp, filename):
+x0rbm = np.array([iters,lrn_rate,log_cval, log_cval2, n_comps])
+res_rbm = minimize(runRBM, x0rbm, args=(0), method='nelder-mead',options={'xtol': 1e-2, 'disp': True})
+print res_rbm.x
+logfile.write(res_rbm.x)
+
+#iters, lrn_rate, logistic_c_val, logistic_c_val2, n_comp, filename):
+x0rbm1 = np.array([iters,lrn_rate,log_cval, log_cval2, n_comps])
+res_rbm1 = minimize(runRBM, x0rbm1, args=(1), method='nelder-mead',options={'xtol': 1e-2, 'disp': True})
+print res_rbm1.x
+logfile.write(res_rbm1.x)
+
+#iters, lrn_rate, logistic_c_val, logistic_c_val2, n_comp, filename):
+x0rbm2 = np.array([iters,lrn_rate,log_cval, log_cval2, n_comps])
+res_rmb2 = minimize(runRBM, x0rbm2, args=(2), method='nelder-mead',options={'xtol': 1e-2, 'disp': True})
+print res_rbm2.x
+logfile.write(res_rbm2.x)
+
 
 logfile.close()
